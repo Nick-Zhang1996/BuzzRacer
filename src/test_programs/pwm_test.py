d37792ed2185c15104ce05af0d5af57e0093fd32
Mayhul Arora <mayhul@lawn-128-61-29-140.lawn.gatech.edu>
2017-10-30 15:04:48 -0400

    Testing wiringpi2 example

--- src/test_programs/pwm_test.py
+++ src/test_programs/pwm_test.py
@@ -6,5 +6,6 @@ import time
 pin = 6
 
 wpi.wiringPiSetup()
-wpi.pinMode(pin, 1)
+wpi.pinMode(pin, 1)       # Set pin 6 to 1 ( OUTPUT )
+wpi.digitalWrite(pin, 1)  # Write 1 ( HIGH ) to pin 6
 print wpi.digitalRead(pin)
