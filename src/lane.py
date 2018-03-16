3b972518e00b4aed8ff9783fcf9c16796334007c
Nick Zhang <nickzhang@gatech.edu>
2018-03-16 14:10:43 -0700

    right shoulder detection

--- src/lane.py
+++ src/lane.py
@@ -9,22 +9,95 @@ x_size = 640
 y_size = 480
 
 def showg(img):
-    plt.imshow(img,cmap='gray')
+    plt.imshow(img,cmap='gray',interpolation='nearest')
     plt.show()
     return
 
+def normalize(data):
+    data = data.astype(np.float32)
+    data = data/255
+    mean = np.mean(data)
+    stddev = np.std(data)
+    data = (data-mean)/stddev
+    return data
+
 # takes a grayscale frame, return a centerline
 def pipeline(frame):
-    frame = frame[220:,:]
-    frame = frame.astype(np.float32)
-    frame = frame/255
-    mean = np.mean(frame)
-    stddev = np.std(frame)
-    frame = (frame-mean)/stddev
+
+    #crop
+    frame = frame[240:,:]
+
+    # normalize
+    frame = normalize(frame)
     showg(frame)
-    frame[frame>1]=1
-    frame[frame<1]=0
-    showg(frame)
+    sobel_threshold(frame)
+
+    #frame[frame>1]=1
+    #frame[frame<1]=0
+
+#direction
+def sobel_threshold(gray, sobel_kernel=7, thresh=(0.6, 1.3)):
+
+    # Calculate the x and y gradients
+    sobelx = cv2.Sobel(gray, cv2.CV_32F, 1, 0, ksize=sobel_kernel)
+    sobely = cv2.Sobel(gray, cv2.CV_32F, 0, 1, ksize=sobel_kernel)
+
+    #graddir = np.arctan2(sobely, sobelx)
+
+    norm = np.sqrt(np.square(sobelx)+np.square(sobely))
+    norm = normalize(norm)
+    showg(norm)
+    #norm > 1 to get good edges
+
+    
+    # find RIGHT line
+    # find left edge of lanes
+    binary_output =  np.zeros_like(gray,dtype=np.uint8)
+    # XXX gray>1.5 is a sketchy solution that cut data size in half
+    binary_output[(gray>1.5)&(sobelx>0) & (norm>1)] = 1
+    #showg(binary_output)
+    
+    #label connected components
+    connectivity = 8 
+    output = cv2.connectedComponentsWithStats(binary_output, connectivity, cv2.CV_32S)
+    # The first cell is the number of labels
+    num_labels = output[0]
+    # The second cell is the label matrix
+    labels = output[1]
+    # The third cell is the stat matrix
+    stats = output[2]
+    # The fourth cell is the centroid matrix
+    centroids = output[3]
+
+    '''
+    # for DEBUG
+    
+    # Map component labels to hue val
+    label_hue = np.uint8(179*labels/np.max(labels))
+    blank_ch = 255*np.ones_like(label_hue)
+    labeled_img = cv2.merge([label_hue, blank_ch, blank_ch])
+
+    # cvt to BGR for display
+    labeled_img = cv2.cvtColor(labeled_img, cv2.COLOR_HSV2BGR)
+
+    # set bg label to black
+    labeled_img[label_hue==0] = 0
+
+    showg(labeled_img)
+    '''
+
+    line_labels = np.argsort(stats[:,cv2.CC_STAT_AREA][1:])[-2:]+1
+    # find the right line
+    if (centroids[line_labels[0]][0]>centroids[line_labels[1]][0]):
+        right_label = line_labels[0]
+    else:
+        right_label = line_labels[1]
+
+    binary_output =  np.zeros_like(gray,dtype=np.uint8)
+    binary_output[labels==right_label]=1
+    showg(binary_output)
+
+    return binary_output
     
     
 def testimg(filename):
@@ -37,6 +110,7 @@ def testimg(filename):
 if __name__ == '__main__':
     print('begin')
     testpics =['../img/0.png','../img/1.png','../img/2.png','../img/3.png','../img/4.png','../img/5.png','../img/6.png','../img/7.png'] 
+    #total 8 pics
     for i in range(8):
         testimg(testpics[i])
 
