1c3ebcc650789b3e60b5e55550c3fc2258299629
Nick Zhang <nickzhang@gatech.edu>
2018-06-21 20:29:03 -0700

    add CarControl, CarSensor msg

--- firmware/arduino_main.cpp
+++ firmware/arduino_main.cpp
@@ -1,6 +1,8 @@
 #include <Arduino.h>
 #include <ros.h>
 #include <std_msgs/Float64.h>
+#include <rc_vip/CarSensors.h>
+#include <rc_vip/CarControl.h>
 #include <Servo.h>
 
 //throttle variables
@@ -23,23 +25,21 @@ static unsigned long steeringTimestamp = 0;
 //ros variables
 ros::NodeHandle nh;
 
-void readThrottleTopic(const std_msgs::Float64 &msg_throttle) {
-    throttleTimestamp = millis();
-    if (msg_throttle.data < 0.05) {
+void readCarControlTopic(const vip_rc::CarControl &msg_CarControl) {
+    carControlTimestamp = millis();
+
+    if (msg_CarControl.throttle < 0.005) {
         throttleServoVal = minThrottleVal;
-    } else if (msg_throttle.data > 1.01) {
+    } else if (msg_CarControl.throttle > 1.0001) {
         throttleServoVal = minThrottleVal;
     } else {
-        throttleServoVal = (int) map( msg_throttle.data, 0.0, 1.0, 1460, 1450);
+        //needs a re-calibration
+        throttleServoVal = (int) map( msg_CarControl.throttle, 0.0, 1.0, 1460, 1450);
     }
     // COMMENT THIS OUT for moving motor
     //throttleServoVal = minThrottleVal;
-    return;
-}
 
-void readSteeringTopic(const std_msgs::Float64 &msg_steerAngle) {
-    steeringTimestamp = millis();
-    float tempSteering = constrain(msg_steerAngle.data, steeringLeftLimit, steeringRightLimit);
+    float tempSteering = constrain(msg_CarControl.steer_angle, steeringLeftLimit, steeringRightLimit);
     if ( tempSteering > 0.0 ){
         steeringServoVal = (int) map(tempSteering, 0.0, steeringRightLimit, 1550, 1900);
     } else if ( tempSteering < 0.0 ){
@@ -48,41 +48,37 @@ void readSteeringTopic(const std_msgs::Float64 &msg_steerAngle) {
         steeringServoVal = 1550;
     }
 
-    return;
+    return
 }
 
 
-
-ros::Subscriber<std_msgs::Float64> subThrottle("throttle", &readThrottleTopic);
-
-ros::Subscriber<std_msgs::Float64> subSteering("steer_angle", &readSteeringTopic);
+ros::Subscriber<rc_vip::CarControl> subCarControl("/carControl", &readCarControlTopic);
 
 void setup() {
-    Serial.begin(9600);
+    Serial.begin(115200);
     nh.initNode();
-    nh.subscribe(subThrottle);
-    nh.subscribe(subSteering);
+    nh.subscribe(subCarControl);
 
     pinMode(pinDrive, OUTPUT);
     pinMode(pinServo, OUTPUT);
     throttle.attach(pinDrive);
     steer.attach(pinServo);
    
-    //setup ESC
+    //ESC requires a low signal durin poweron to prevent accidental input
     throttle.writeMicroseconds(minThrottleVal);
     delay(5000);
 }
 
 void loop() {
     nh.spinOnce();
-    if ( millis() - throttleTimestamp > 500 || millis() - steeringTimestamp > 500 ){
+    //failsafe, if there's no new message for over 500ms, halt the motor
+    if ( millis() - carControlTimestamp > 500 ){
         throttle.writeMicroseconds(minThrottleVal);
-    }   
+    } else {  
+        throttle.writeMicroseconds(throttleServoVal);
+        steer.writeMicroseconds(steeringServoVal);
+    }
 
-
-    
-       
-    throttle.writeMicroseconds(throttleServoVal);
-    steer.writeMicroseconds(steeringServoVal);
+    // a loop rate too high may mess with Servo class's operation
     delay(10);
 }
