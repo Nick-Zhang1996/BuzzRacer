93909643fb7b784928fa5eebc2e041dc56455e20
jylty3 <jylty3@gatech.edu>
2019-10-04 16:43:50 -0400

    added vicon interpreter

---
+++ src/python_udp.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+import rospy
+import socket
+from rc_vip.msg import Vicon
+
+
+def vicon(pub_vicon):
+	IP = ''
+	PORT = 3883
+	sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	sock.bind((IP, PORT))
+	data, addr = sock.recvfrom(1024)
+	data = data.strip('[]')
+	data = data.split(' ')
+	pub_data = []
+	for d in data:
+		pub_data.append(float(d))
+	print("received message:", pub_data)
+	pub_vicon.publish(pub_data)
+
+if __name__ == '__main__':
+	rospy.init_node("udp_listener")
+	pub_vicon = rospy.Publisher("/vicon_tf", Vicon, queue_size=10)
+	while not rospy.is_shutdown():	
+		vicon(pub_vicon)
