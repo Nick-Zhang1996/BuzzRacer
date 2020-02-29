8729d7633d6f44f2a9caf873c95f6235abaf3bce
Nick Zhang <zzhang615@gatech.edu>
2020-02-29 18:38:44 -0500

    sample vicon data

--- src/vicon.py
+++ src/vicon.py
@@ -10,7 +10,7 @@ class Vicon:
         if IP is None:
             IP = "0.0.0.0"
         if PORT is None:
-            PORT = 51001
+            PORT = 51001+4
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.sock.bind((IP, PORT))
         
@@ -19,6 +19,8 @@ class Vicon:
 
     def getViconUpdate(self):
         data, addr = self.sock.recvfrom(256)
+        print(data)
+        self.save2File(data,"twoobj.vicon")
         # flush
         #while (len(data)==512):
         #    data, addr = self.sock.recvfrom(512)
@@ -38,6 +40,7 @@ class Vicon:
         ry = unpack('d',data[64:72])[0]
         rz = unpack('d',data[72:80])[0]
         #print(x,y,z,degrees(rx),degrees(ry),degrees(rz))
+        print(itemsInBlock,itemID,itemName)
         return (x,y,z,rx,ry,rz)
 
     def testFreq(self,packets=100):
@@ -49,9 +52,10 @@ class Vicon:
         return packets/(tac-tic)
 
     # for debug
-    def fromFile(self,filename):
+    def save2File(self,data,filename):
         newFile = open(filename, "wb")
         newFile.write(data)
+        newFile.close()
 
 
 if __name__ == '__main__':
@@ -61,10 +65,13 @@ if __name__ == '__main__':
     tik = time()
     last_frame = None
     loss_count = 0
+    vi.getViconUpdate()
+    exit(0)
     while True:
-        print(vi.getViconUpdate())
+        #print(vi.getViconUpdate())
+        vi.getViconUpdate()
     # test freq
-    if True:
+    if False:
         for i in range(3):
             print("Freq = "+str(vi.testFreq())+"Hz")
 
