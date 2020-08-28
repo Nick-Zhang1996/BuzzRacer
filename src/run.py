b2601beb14052f929e29d63fefcfe1e2b55068f5
Nick Zhang <zzhang615@gatech.edu>
2020-08-28 18:27:31 -0400

    add full rcp track

--- src/run.py
+++ src/run.py
@@ -184,7 +184,7 @@ class Main():
         sp.initSkidpad(radius=2,velocity=target_velocity)
         return sp
 
-    def prepareRcpTrack(self,):
+    def prepareRcpTrackSmall(self,):
         # current track setup in mk103, L shaped
         # width 0.563, length 0.6
         mk103 = RCPtrack()
@@ -197,6 +197,50 @@ class Main():
         adjustment[10] = -0.5
         mk103.initRaceline((2,2),'d',4,offset=adjustment)
         return mk103
+
+    def prepareRcpTrack(self,):
+        # current track setup in mk103, L shaped
+        # TODO change dimension
+        # width 0.563, square length 0.6
+
+        # full RCP track
+        # row, col
+        fulltrack = RCPtrack()
+        track_size = (6,4)
+        #fulltrack.initTrack('uuurrullurrrdddddluulddl',track_size, scale=0.565)
+        fulltrack.initTrack('uuurrullurrrdddddluulddl',track_size, scale=0.6)
+        # add manual offset for each control points
+        adjustment = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+
+        adjustment[0] = -0.2
+        adjustment[1] = -0.2
+        #bottom right turn
+        adjustment[2] = -0.2
+        adjustment[3] = 0.5
+        adjustment[4] = -0.2
+
+        #bottom middle turn
+        adjustment[6] = -0.2
+
+        #bottom left turn
+        adjustment[9] = -0.2
+
+        # left L turn
+        adjustment[12] = 0.5
+        adjustment[13] = 0.5
+
+        adjustment[15] = -0.5
+        adjustment[16] = 0.5
+        adjustment[18] = 0.5
+
+        adjustment[21] = 0.35
+        adjustment[22] = 0.35
+
+        # start coord, direction, sequence number of origin
+        # pick a grid as the starting grid, this doesn't matter much, however a starting grid in the middle of a long straight helps
+        # to find sequence number of origin, start from the start coord(seq no = 0), and follow the track, each time you encounter a new grid it's seq no is 1+previous seq no. If origin is one step away in the forward direction from start coord, it has seq no = 1
+        fulltrack.initRaceline((3,3),'d',10,offset=adjustment)
+        return fulltrack
 
     def prepareCar(self,):
         porsche_setting = {'wheelbase':90e-3,
