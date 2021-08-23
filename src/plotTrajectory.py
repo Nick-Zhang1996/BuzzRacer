1fc25dcb97caeabff85c88b06e6fd44827afcdeb
Nick Zhang <zzhang615@ae.gatech.edu>
2021-08-23 18:24:07 -0400

    plot all in one

--- src/plotTrajectory.py
+++ src/plotTrajectory.py
@@ -5,28 +5,33 @@ import matplotlib.pyplot as plt
 from TrackFactory import TrackFactory
 import cv2
 
+def plotTraj(filename, img, color):
+    with open(filename, 'rb') as f:
+        data = pickle.load(f)
+    data = np.array(data).squeeze(1)
+
+    x = data[:,1]
+    y = data[:,2]
+    points = np.vstack([x,y]).T
+    track.drawPolyline(points, img, lineColor=color,thickness=2)
+    return img
+
+
 # load blank canvas
 with open("track_img.p", 'rb') as f:
     img = pickle.load(f)
 
-# load log
-if (len(sys.argv) != 2):
-    filename = "../log/kinematics_results/full_state10.p"
-    print_warning("reading default file" + filename)
-else:
-    filename = sys.argv[1]
+filename = "../log/kinematics_results/full_state8.p"
+img = plotTraj(filename, img, (255,0,0))
 
-with open(filename, 'rb') as f:
-    data = pickle.load(f)
-data = np.array(data).squeeze(1)
+filename = "../log/kinematics_results/full_state9.p"
+img = plotTraj(filename, img, (0,255,0))
 
-x = data[:,1]
-y = data[:,2]
-points = np.vstack([x,y]).T
+filename = "../log/kinematics_results/full_state10.p"
+img = plotTraj(filename, img, (0,0,255))
 
 track = TrackFactory(name='full')
 grayness = 180
-track.drawPolyline(points, img, lineColor=(grayness,grayness,grayness),thickness=2)
 img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
 plt.imshow(img)
