33281f04528a2648ab3df4de3c82df96656d1dfb
Nick Zhang <zzhang615@ae.gatech.edu>
2021-08-24 10:47:52 -0400

    plot all traj on one plot

--- src/plotTrajectory.py
+++ src/plotTrajectory.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 from TrackFactory import TrackFactory
 import cv2
 
-def plotTraj(filename, img, color):
+def plotTraj(track, filename, img, color):
     with open(filename, 'rb') as f:
         data = pickle.load(f)
     data = np.array(data).squeeze(1)
@@ -21,17 +21,22 @@ def plotTraj(filename, img, color):
 with open("track_img.p", 'rb') as f:
     img = pickle.load(f)
 
-filename = "../log/kinematics_results/full_state8.p"
-img = plotTraj(filename, img, (255,0,0))
-
-filename = "../log/kinematics_results/full_state9.p"
-img = plotTraj(filename, img, (0,255,0))
-
-filename = "../log/kinematics_results/full_state10.p"
-img = plotTraj(filename, img, (0,0,255))
 
 track = TrackFactory(name='full')
 grayness = 180
+
+filename = "../log/kinematics_results/full_state8.p"
+img1 = plotTraj(track,filename, img.copy(), (255,0,0))
+
+filename = "../log/kinematics_results/full_state9.p"
+img2 = plotTraj(track,filename, img.copy(), (0,255,0))
+
+filename = "../log/kinematics_results/full_state10.p"
+img3 = plotTraj(track,filename, img.copy(), (0,0,255))
+
+img = (img1/3 + img2/3 + img3/3)
+img = np.array(img,dtype=np.uint8)
+
 img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
 plt.imshow(img)
