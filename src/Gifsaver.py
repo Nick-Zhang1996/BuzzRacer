ac6fb7a30b34d04477a7919b46dac046ba772515
Nick Zhang <zzhang615@ae.gatech.edu>
2021-09-18 15:16:28 -0400

    minor experimental setting change

--- src/Gifsaver.py
+++ src/Gifsaver.py
@@ -41,6 +41,6 @@ class Gifsaver(Extension):
         print_ok("[Gifsaver]: saving gif.. This may take a while")
         gif_filename = "../gifs/sim"+str(self.log_no)+".gif"
         # TODO better way of determining duration
-        self.gifimages[0].save(fp=gif_filename,format='GIF',append_images=self.gifimages,save_all=True,duration = 30,loop=0)
+        self.gifimages[0].save(fp=gif_filename,format='GIF',append_images=self.gifimages,save_all=True,duration = 50,loop=0)
         print_ok("[Gifsaver]: gif saved at "+gif_filename)
 
