2e24539843869de3f84ccb1501eaa3b0cf60e322
Nick Zhang <zzhang615@ae.gatech.edu>
2021-10-25 20:29:23 -0400

    more gurobi function

--- src/ccmppi/gurobi_test.py
+++ src/ccmppi/gurobi_test.py
@@ -2,13 +2,17 @@
 import gurobipy as gp
 from gurobipy import GRB
 from gurobipy import *
-
 import numpy as np
 
+def vec(var):
+    return vec.flatten('F')
+def unvec(var,shape):
+    return var.reshape(shape,order='F')
 # express tr(AXB) in gurobi acceptable form
 # A: p*m np matrix
 # B: n*p np matrix
 # vecX: vec(x), (m*n)*1 gurobi vector (1D)
+# return: gurobi expression
 def gurobi_trAXB(A,B,vecX):
     assert (A.shape[0] == B.shape[1])
     assert (vecX.shape[0] == A.shape[1]*B.shape[0])
@@ -32,7 +36,46 @@ def gurobi_trAXB_alt(A,B,vecX):
     retval = sum((val[i] * vecX[i]) for i in range(n*m))
     return retval
 
-def test_fun(A,B,name,fun):
+# express tr(A.T @ X.T @ Q @ X @ A)
+# A: n*p, np matrix
+# Q_sqrt: sqrt(Q), m*m, np matrix
+# vecX: vec(X) X:m*n
+# return: gurobi expression
+def gurobi_matrix_quad(A, Q_sqrt, vecX):
+    temp = np.kron(A.T, Q_sqrt)
+    return vecX @ (temp.T @ temp) @ vecX
+
+def test_quad_fun():
+    m = 2
+    n = 3
+    p = 3
+    A = np.random.rand(n,p)
+    # random symmetric matrix
+    Q_sqrt = np.random.rand(m,m)
+    Q_sqrt = Q_sqrt + Q_sqrt.T
+
+    name = "quad"
+    model = gp.Model(name)
+    # supress output
+    model.setParam(GRB.Param.OutputFlag, 0)
+    vecX = model.addMVar(shape=(m*n), name='vecX')
+    obj = gurobi_matrix_quad(A,Q_sqrt,vecX)
+    model.addConstr( sum(vecX[i] for i in range(m*n)) == 1)
+    model.setObjective(obj, GRB.MINIMIZE)
+    model.optimize()
+    print("decision variable")
+    print(vecX.x)
+    print("objective function")
+    print(model.ObjVal)
+
+    X = unvec(vecX.x,(m,n))
+    obj_val = np.trace(A.T @ X.T @ Q_sqrt @ Q_sqrt @ X @ A)
+    assert(model.ObjVal - obj_val < 0.0001)
+    return
+
+
+
+def test_AXB_fun(A,B,name,fun):
     p = A.shape[0]
     m = A.shape[1]
     n = B.shape[0]
@@ -66,12 +109,11 @@ def test_fun(A,B,name,fun):
     assert(original - form3 < 0.001)
     assert(original - form4 < 0.001)
 
-
     print("objective function real")
     print(original)
     print('-------')
 
-if __name__=="__main__":
+if __name__=="__1main__":
     m = 2
     n = 3
     p = 3
@@ -81,7 +123,9 @@ if __name__=="__main__":
     print(A)
     print("B")
     print(B)
-    test_fun(A,B,"1",gurobi_trAXB)
-    test_fun(A,B,"2",gurobi_trAXB_alt)
+    test_AXB_fun(A,B,"1",gurobi_trAXB)
+    test_AXB_fun(A,B,"2",gurobi_trAXB_alt)
 
+if __name__=="__main__":
+    test_quad_fun()
 
