960870cdd5ecb6c46cdcaab51c0c6b41d810a3ad
Nick Zhang <zzhang615@ae.gatech.edu>
2022-08-31 15:00:33 -0400

    batch config generator

---
+++ src/configs/batch_example/configGenerator.py
@@ -0,0 +1,29 @@
+# Example: load a config, modify as needed, then save xml
+# to run batch experiments, you should write a file like this to generate all configs needed, then call 
+# python batchExperiment.py folder_of_config
+from xml.dom import minidom
+import xml.etree.ElementTree as ET
+import numpy as np
+from copy import deepcopy
+
+config_filename = 'master.xml'
+original_config = minidom.parse(config_filename)
+
+index = 0
+cvar_a_vec = np.linspace(0.1,0.9,9)
+cvar_Cu_vec = [0,1,5,10,20,30]
+
+for cvar_a in cvar_a_vec:
+    for cvar_Cu in cvar_Cu_vec:
+        config = deepcopy(original_config)
+        # get cvar_a
+        config_cars = config.getElementsByTagName('cars')[0]
+        config_car = config_cars.getElementsByTagName('car')[0]
+        config_controller = config_car.getElementsByTagName('controller')[0]
+        attrs = config_controller.attributes.items()
+
+        config_controller.attributes['cvar_Cu'] =  str(cvar_Cu)
+        config_controller.attributes['cvar_a'] =   str(cvar_a)
+        with open('exp%d.xml'%(index),'w') as f:
+            config.writexml(f)
+        index += 1
