c3305797fcfde18ec681c86c4bba0d721cb98760
Nick Zhang <nickzhang@gatech.edu>
2022-12-07 16:03:40 -0500

    update var name

--- src/copg/copg_optim/critic_functions.py
+++ src/copg/copg_optim/critic_functions.py
@@ -14,14 +14,14 @@ def critic_update(state_mb, return_mb, q, optim_q):
 
     yield critic_loss_numpy, 1
 
-def get_advantage(next_value, reward_mat, value_mat, masks, gamma=0.99, tau=0.95,device=None):
+def get_advantage(next_value, reward_mat, value_mat, done, gamma=0.99, tau=0.95,device=None):
     next_value_torch = torch.tensor([[next_value]],dtype=torch.float,device=device)
     value_mat = torch.cat([value_mat, next_value_torch])
     gae = 0
     returns = []
 
     for step in reversed(range(len(reward_mat))):
-        delta = reward_mat[step] + gamma * value_mat[step + 1] * masks[step] - value_mat[step]
-        gae = delta + gamma * tau * masks[step] * gae
+        delta = reward_mat[step] + gamma * value_mat[step + 1] * done[step] - value_mat[step]
+        gae = delta + gamma * tau * done[step] * gae
         returns.insert(0, gae + value_mat[step])
     return returns
