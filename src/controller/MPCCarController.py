61d88ecda0337cd0a099c0b82f2f172f66e128d4
Cody Nelson <cnelson304@gatech.edu>
2023-03-29 15:08:56 -0400

    parameter change

--- src/controller/MPCCarController.py
+++ src/controller/MPCCarController.py
@@ -75,8 +75,8 @@ class MPCCarController(CarController):
         self.G = np.zeros((constraint_count, self.l)) #Gx <= h (element-wise). x is n x 1, G needs to be q x n, h needs to be q x 1, where q is the number of constraints
         self.H = np.zeros((constraint_count, 1))
 
-        maxSteeringInput = 30
-        maxThrottleInput = 20
+        maxSteeringInput = 30 / 3
+        maxThrottleInput = 20 / 3
 
         # ds/dt < 1
         self.G[0][0] = 1
