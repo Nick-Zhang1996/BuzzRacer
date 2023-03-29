8985d72bca6e83c1d764ff0d1bd91d66c8af894d
Nick Zhang <zzhang615@ae.gatech.edu>
2023-03-29 14:56:13 -0400

    experiment xml file and update lookahead

--- src/controller/MPCCarController.py
+++ src/controller/MPCCarController.py
@@ -16,7 +16,7 @@ class MPCCarController(CarController):
         cvxopt.solvers.options['show_progress'] = False
 
         self.N = 5 #horizon
-        self.look_ahead = 0.2
+        self.look_ahead = 0.1
         self.v_target_multiplier = 0.925 #0.95 = safe, 1 = aggresive, >1 = spin out
 
         self.dt = self.look_ahead / self.N
