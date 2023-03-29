331f144f5105f1c430bd2645592a8b7c970815f7
Cody Nelson <cnelson304@gatech.edu>
2023-03-29 17:40:26 -0400

    experiment

--- src/controller/MPCCarController.py
+++ src/controller/MPCCarController.py
@@ -15,8 +15,8 @@ class MPCCarController(CarController):
 
         cvxopt.solvers.options['show_progress'] = False
 
-        self.N = 10 #horizon
-        self.look_ahead = 0.35
+        self.N = 8 #horizon
+        self.look_ahead = 0.5
         self.v_target_multiplier = 0.96 #0.95 = safe, 1 = aggresive, >1 = spin out
 
         self.dt = self.look_ahead / self.N
