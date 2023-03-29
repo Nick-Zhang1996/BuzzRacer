21dadc22a93966c6cee34493df1c21fec437c717
Cody Nelson <cnelson304@gatech.edu>
2023-03-29 15:56:00 -0400

    exp

--- src/controller/MPCCarController.py
+++ src/controller/MPCCarController.py
@@ -195,7 +195,7 @@ class MPCCarController(CarController):
         #NOTE: in simulation, this should be 1. Physical is different for some reason? Need to remove this anyways
         A[i["r"]][i["ds"]] = (1 if self.simulation else 2) * dt / i_z #NOT ACCURATE TO THE MODEL, BUT IT COMMUNICATES A LINEAR RELATIONSHIP BETWEEN STEERING AND ROTATIONAL SPEED. having a little bit of this makes it work better!
 
-        A[i["vx"]][i["dt"]] = (dt) * 6.17 * (1 if self.simulation else 13)
+        A[i["vx"]][i["dt"]] = (dt) * 6.17 * (1 if self.simulation else 2)
         #A[i["vx"]][i["vx"]] = -(dt) * 6.17 / 15.2        THIS SHOULD BE HERE BUT IT BREAKS IT
         A[i["vx"]][i["one"]] = -(dt) * 6.17 / 3
         
