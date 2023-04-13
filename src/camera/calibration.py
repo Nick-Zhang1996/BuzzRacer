a5ecc1746622658e0f8fae38a16fc35a7b6c6e0e
Nick Zhang <nickzhang@gatech.edu>
2023-04-13 16:49:16 -0400

    good calibration

---
+++ src/camera/calibration.py
@@ -0,0 +1,54 @@
+# camera calibration from views of a chessboard pattern
+import glob
+import numpy as np
+import cv2 as cv
+import matplotlib.pyplot as plt
+import pickle
+
+# prepare object points
+dim = (9,7)
+objp = np.zeros((dim[1]*dim[0],3), np.float32)
+objp[:,:2] = np.mgrid[0:dim[0],0:dim[1]].T.reshape(-1,2)
+#objp = objp*0.021
+criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+
+#image_filename = './calibration/IMG_7027.JPG'
+#img = cv.imread(image_filename)
+image_filename_vec = glob.glob('./calibration/*.JPG')
+
+print(f'reading images...')
+for fname in image_filename_vec:
+    img = cv.imread(fname)
+    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    # Find the chess board corners
+    ret, corners = cv.findChessboardCorners(gray, dim, None)
+    # If found, add object points, image points (after refining them)
+    objpoints = []
+    imgpoints = []
+    if ret == True:
+        objpoints.append(objp)
+        # refind corners
+        corners2 = cv.cornerSubPix(gray,corners, (11,11), (-1,-1), criteria)
+        imgpoints.append(corners2)
+        # Draw and display the corners
+        cv.drawChessboardCorners(img, dim, corners2, ret)
+
+        # show images
+        #plt_img = cv.cvtColor(img,cv.COLOR_BGR2RGB)
+        #plt.imshow(plt_img)
+        #plt.show()
+
+# Calibration
+print(f'calibrating...')
+ret, mtx, dist, rvecs, tvecs = cv.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
+print(f'residual: {ret}')
+print(f'mtx: {mtx}')
+
+with open('camera.p','wb') as f:
+    pickle.dump([ret, mtx, dist, rvecs, tvecs],f)
+
+# Undistortion
+#img = cv.imread('left12.jpg')
+#h,  w = img.shape[:2]
+#newcameramtx, roi = cv.getOptimalNewCameraMatrix(mtx, dist, (w,h), 1, (w,h))
+
