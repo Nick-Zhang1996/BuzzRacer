45b18d52b211dd98bcdac5f85dd3011e6f5c182c
Nick Zhang <nickzhang@gatech.edu>
2023-04-13 23:41:04 -0400

    bad performance transformation via rvecs and tvecs

--- src/camera/transformation.py
+++ src/camera/transformation.py
@@ -11,15 +11,31 @@ with open('camera.p','rb') as f:
 print(f'residual: {ret}')
 print(f'mtx: {mtx}')
 
+objpoints = [[3,1,0],[7,1,0],[5,7,0],[1,11,0],[7,11,0]]
+imgpoints = [[1551,618],[1438,279],[914,366],[267,850],[462,131]]
+
+
+objpoints = np.array(objpoints, dtype=float)
+imgpoints = np.array(imgpoints, dtype=float)
+
+# load track
+img = cv.imread('resources/track.png')
+objpoints = [objpoints[i] for i in (0,1,3,4)]
+imgpoints = [imgpoints[i] for i in (0,1,3,4)]
+
+objpoints = np.array(objpoints, dtype=float)
+imgpoints = np.array(imgpoints, dtype=float)
+
+ret, rvec, tvec = cv.solvePnP(objpoints, imgpoints, mtx, dist, flags=cv.SOLVEPNP_P3P)
+print(f'PnP residual {ret}')
+
 # test transformation
-image_filename_vec = glob.glob('./calibration/*.JPG')
-
-src = np.array([[4.5,3.5,0]])
-dst, _ = cv.projectPoints(src, rvecs[0], tvecs[0], mtx, dist)
-dst = tuple(dst.flatten().astype(int))
-
-img = cv.imread(image_filename_vec[0])
-cv.circle(img,dst, 5, (0,0,255), -1)
+for objpoint in objpoints:
+    src = np.array([objpoint],dtype=float)
+    dst, _ = cv.projectPoints(src, rvec, tvec, mtx, dist)
+    dst = tuple(dst.flatten().astype(int))
+    cv.circle(img,dst, 15, (0,0,255), -1)
+    print(dst)
 
 plt_img = cv.cvtColor(img,cv.COLOR_BGR2RGB)
 plt.imshow(plt_img)
