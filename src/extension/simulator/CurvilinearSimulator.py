c864380ac4bc2c827d7b9751434e9d7d9f1b0456
Nick Zhang <zzhang615@ae.gatech.edu>
2024-01-10 14:19:35 -0500

    fix identical progress bug in config generation

--- src/extension/simulator/CurvilinearSimulator.py
+++ src/extension/simulator/CurvilinearSimulator.py
@@ -78,7 +78,12 @@ class CurvilinearSimulator(Simulator):
             return val
 
         if (guess_s is None):
-            fit = minimize(dist, x0=0, method='L-BFGS-B', bounds=((-1.0,self.main.track.raceline_len_m),))
+            # initial guess to avoid local minima
+            xx = np.linspace(0.0, self.track.raceline_len_m,10)
+            yy = [dist(x) for x in xx]
+            guess_s = xx[np.argmin(yy)]
+            ds = 2*self.track.raceline_len_m/10
+            fit = minimize(dist, x0=guess_s, method='L-BFGS-B', bounds=((guess_s-ds,guess_s+ds),))
         else:
             fit = minimize(dist, x0=guess_s, method='L-BFGS-B', bounds=((guess_s-0.2,guess_s+0.2),))
 
@@ -92,6 +97,22 @@ class CurvilinearSimulator(Simulator):
         v = v_forward
         phi = wrap(heading - np.arctan2(dr[1],dr[0]))
         return np.array([s,v,n,phi])
+
+    # DEBUG
+    def debugPlot(self,cart):
+        x,y,heading,v_forward,v_sideway,omega = cart
+        def dist(s):
+            val = np.linalg.norm(np.array(splev(s%self.track.raceline_len_m,self.track.raceline_s,der=0)).flatten() - np.array([x,y]))
+            return val
+        xx = np.linspace(-1.0, self.track.raceline_len_m,1000)
+        yy = [dist(x) for x in xx]
+        plt.plot(xx,yy)
+
+        xx = np.linspace(-1.0, self.track.raceline_len_m,10)
+        yy = [dist(x) for x in xx]
+        plt.plot(xx,yy,'o')
+        plt.show()
+        return
 
     def curv2Cart(self, curv):
         '''
