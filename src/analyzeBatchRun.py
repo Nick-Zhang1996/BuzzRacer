e16b3e433c58327aebf577e7f8eb8b66531f6f2c
Nick Zhang <nickzhang@gatech.edu>
2024-01-11 16:16:35 -0500

    analysis for evasive controller

--- src/analyzeBatchRun.py
+++ src/analyzeBatchRun.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 labels = "experiment_name , config_file_name , log_name , laps , Qop1, Qop2, start_lead_i_j, end_lead_i_j, laptime_mean , laptime_stddev , boundary_violation , obstacle_violation"
 labels = [val.strip() for val in labels.split(',')]
 
-log = '../log/batch_ilqgame_solo/new_textlog.txt'
+log = '../log/batch_ilqgame_vs_evasive/textlog.txt'
 print('opening log ' + log)
 failed_runs_count = 0
 with open(log,'r') as f:
