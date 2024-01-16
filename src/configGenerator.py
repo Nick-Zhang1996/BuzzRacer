a548237ae85c2eb52196ddbbbc334b53066ede11
Nick Zhang <zzhang615@ae.gatech.edu>
2024-01-16 16:16:36 -0500

    add blocking experiment

--- src/configGenerator.py
+++ src/configGenerator.py
@@ -35,7 +35,9 @@ sim.track = track
 
 def getRandomInitialStatePair():
     s0 = np.random.uniform(0.4,track.raceline_len_m-0.4)
-    s1 = s0 + np.random.uniform(-0.3,0.3)
+    #s1 = s0 + np.random.uniform(-0.3,0.3)
+    # s1 in rear
+    s1 = s0 + np.random.uniform(-0.5,-0.3)
     n0 = np.random.uniform(-0.2,0.2)
     n1 = np.random.uniform(-0.2,0.2)
     drr0 = splev(s0,track.raceline_s,der=1)
@@ -44,8 +46,11 @@ def getRandomInitialStatePair():
     drr1 = splev(s1,track.raceline_s,der=1)
     heading1 = np.arctan2(drr0[1],drr0[0])
     '''
-    v0 = np.random.uniform(3.0,4.0)
-    v1 = np.random.uniform(3.0,4.0)
+    #v0 = np.random.uniform(3.0,4.0)
+    #v1 = np.random.uniform(3.0,4.0)
+    v0 = np.random.uniform(2.5,3.5)
+    # s1(follower) faster
+    v1 = v0 + np.random.uniform(0.2,0.8)
     x0 = np.array((s0,v0,n0,0))
     x1 = np.array((s1,v1,n1,0))
     delta_x = x0 - x1
@@ -62,10 +67,11 @@ def getRandomInitialStatePair():
         return tuple(cart0[:4]),tuple(cart1[:4])
 
 index = 0
-Qop = [0]
+Qop = [4,0,-4]
+# car_i in front
+# car_j in rear, with speed advantage
 
-
-for i in range(30):
+for i in range(50):
     for q0 in Qop:
         for q1 in Qop:
             s0,s1 = getRandomInitialStatePair()
