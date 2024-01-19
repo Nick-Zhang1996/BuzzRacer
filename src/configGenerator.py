a281f361095e0d06051716c79895786c7aa4515b
Nick Zhang <nickzhang@gatech.edu>
2024-01-18 20:52:34 -0500

    blocking and chasing with more advantage to chaser

--- src/configGenerator.py
+++ src/configGenerator.py
@@ -34,12 +34,13 @@ sim = CurvilinearSimulator(FakeMain())
 sim.track = track
 
 def getRandomInitialStatePair():
-    s0 = np.random.uniform(0.5,track.raceline_len_m-0.5)
+    #s0 = np.random.uniform(0.5,track.raceline_len_m-0.5)
     #s1 = s0 + np.random.uniform(-0.3,0.3)
     # s1 in rear
-    s1 = s0 + np.random.uniform(-0.5,-0.3)
+    #s1 = s0 + np.random.uniform(-0.5,-0.3)
     # s1 in front
-    #s1 = s0 + np.random.uniform(0.3,0.5)
+    s0 = np.random.uniform(0.5,track.raceline_len_m-0.5)
+    s1 = s0 + np.random.uniform(0.3,0.5)
 
     n0 = np.random.uniform(-0.2,0.2)
     n1 = np.random.uniform(-0.2,0.2)
@@ -49,14 +50,10 @@ def getRandomInitialStatePair():
     drr1 = splev(s1,track.raceline_s,der=1)
     heading1 = np.arctan2(drr0[1],drr0[0])
     '''
-    #v0 = np.random.uniform(3.0,4.0)
-    #v1 = np.random.uniform(3.0,4.0)
     #v1 = np.random.uniform(2.5,3.0)
-    # v0 (follower) faster
-    #v0 = v1 + np.random.uniform(0.7,1.0)
 
-    v0 = np.random.uniform(2.5,3.0)
-    v1 = v0 + np.random.uniform(0.7,1.0)
+    v1 = np.random.uniform(2.5,3.0)
+    v0 = v1 + np.random.uniform(0.7,1.0)
 
     x0 = np.array((s0,v0,n0,0))
     x1 = np.array((s1,v1,n1,0))
