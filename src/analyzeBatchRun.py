928f58c02821b0ed6f43fcbcd1b8918eae8dbb8d
Nick Zhang <zzhang615@ae.gatech.edu>
2024-02-01 13:59:16 -0500

    chasing/blocking 10% advantage

--- src/analyzeBatchRun.py
+++ src/analyzeBatchRun.py
@@ -7,7 +7,7 @@ from track import TrackFactory
 labels = "experiment_name , config_file_name , log_name , laps , Qop1, Qop2, start_lead_i_j, end_lead_i_j, laptime_mean , laptime_stddev , boundary_violation , obstacle_violation"
 labels = [val.strip() for val in labels.split(',')]
 
-name = 'batch_ilqgame_chasing_boost'
+name = 'blocking_exploit'
 config_folder = './configs/' + name + '/'
 config_filename = config_folder + 'master.xml'
 original_config = minidom.parse(config_filename)
@@ -16,6 +16,7 @@ config_track= original_config.getElementsByTagName('track')[0]
 track = TrackFactory.build(main=None,config=config_track)
 track.init()
 
+# Load textlog.txt and config xml 
 log = '../log/'+name+'/textlog.txt'
 print('opening log ' + log)
 failed_runs_count = 0
@@ -26,6 +27,7 @@ with open(log,'r') as f:
     total_runs = len(text)
     for label in labels:
         data_dict[label] = []
+    data_dict['Qop1_blocking'] = []
     for line in text:
         entry = line.split(',')
         if (eval(entry[-1]) == -1):
@@ -37,11 +39,23 @@ with open(log,'r') as f:
             except (NameError,SyntaxError):
                 datum = raw
             data_dict[label].append(datum)
+        config_filename = entry[1]
+        config = minidom.parse(config_filename)
+        config_cars = config.getElementsByTagName('cars')[0]
+        config_car0 = config_cars.getElementsByTagName('car')[0]
+        config_car1 = config_cars.getElementsByTagName('car')[1]
+        config_controller = config_car0.getElementsByTagName('controller')[0]
+        Qop1_blocking = eval(config_controller.attributes['Qop1_blocking'].nodeValue)
+        data_dict['Qop1_blocking'].append(Qop1_blocking)
+
 print(f'failed runs: {failed_runs_count}, total runs: {total_runs}')
 
 for key in data_dict.keys():
     if (not isinstance(data_dict[key][0],str)):
         data_dict[key] = np.array(data_dict[key])
+
+# Analyze data
+
 data_dict['start_lead_i_j'] = (data_dict['start_lead_i_j'] + track.raceline_len_m/2)%track.raceline_len_m - track.raceline_len_m/2
 data_dict['end_lead_i_j'] = (data_dict['end_lead_i_j'] + track.raceline_len_m/2)%track.raceline_len_m - track.raceline_len_m/2
 
@@ -125,3 +139,25 @@ printTable5Entry(-4,4)
 
 zero_lead = np.abs(data_dict['start_lead_i_j'])<1e-5
 print(f'{np.sum(zero_lead)} zero leads')
+
+# Table 6: analyze blocking controller performance
+def printTable6Entry(qop):
+    mask = data_dict['Qop1_blocking'] == qop
+    follow_mask = np.logical_and( mask , data_dict['start_lead_i_j']<0 )
+    follow_win_mask = np.logical_and( follow_mask , data_dict['end_lead_i_j']>0 )
+    follow_win_ratio = np.sum(follow_win_mask)/np.sum(follow_mask)
+
+    lead_mask = np.logical_and( mask , data_dict['start_lead_i_j']>0 )
+    lead_win_mask = np.logical_and( lead_mask , data_dict['end_lead_i_j']>0 )
+    lead_win_ratio = np.sum(lead_win_mask)/np.sum(lead_mask)
+
+    overall_win_mask = np.logical_and( mask , data_dict['end_lead_i_j']>0 )
+    overall_win_ratio = np.sum(overall_win_mask)/np.sum(mask)
+    print(f'{qop} \t, {follow_win_ratio:.2f}\t\t,{lead_win_ratio:.2f}\t\t,{overall_win_ratio:.2f}\t\t,{np.sum(mask)}')
+
+print('Qop=3')
+printTable6Entry(3)
+print('Qop=-3')
+printTable6Entry(-3)
+print('No exploiting')
+printTable6Entry(-100)
