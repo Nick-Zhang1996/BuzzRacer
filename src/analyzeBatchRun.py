98585e240ea6ff6c638cc255f52fb71967073274
Nick Zhang <zzhang615@ae.gatech.edu>
2024-02-27 11:02:20 -0500

    add analysis script for _dense experiments

--- src/analyzeBatchRun.py
+++ src/analyzeBatchRun.py
@@ -10,7 +10,7 @@ if (len(sys.argv) == 2):
 else:
     print_error("you must specify a folder name under configs/")
 
-labels = "experiment_name , config_file_name , log_name , laps , Qop1, Qop2, start_lead_i_j, end_lead_i_j, laptime_mean_i , boundary_violation_i ,laptime_mean_j , boundary_violation_j , opponent_col"
+labels = "experiment_name , config_file_name , log_name , laps , Qop1_obs, Qop2_obs, start_lead_i_j, end_lead_i_j, laptime_mean_i , boundary_violation_i ,laptime_mean_j , boundary_violation_j , opponent_col"
 labels = [val.strip() for val in labels.split(',')]
 
 config_folder = './configs/' + name + '/'
@@ -33,14 +33,17 @@ with open(log,'r') as f:
     for label in labels:
         data_dict[label] = []
     #data_dict['Qop1_blocking'] = []
-    data_dict['controller_name'] = []
+    data_dict['car0_controller_name'] = []
+    data_dict['car1_controller_name'] = []
     data_dict['blocking_control'] = []
     # alpha, aggressiveness
     data_dict['a0'] = []
     data_dict['a1'] = []
     # Qop1, Qop2 of car0
-    data_dict['Qop1'] = []
-    data_dict['Qop2'] = []
+    data_dict['car0_Qop1'] = []
+    data_dict['car0_Qop2'] = []
+    data_dict['car1_Qop1'] = []
+    data_dict['car1_Qop2'] = []
     for line in text:
         entry = line.split(',')
         if (eval(entry[-1]) == -1):
@@ -62,10 +65,30 @@ with open(log,'r') as f:
         #Qop1_blocking = eval(config_controller.attributes['Qop1_blocking'].nodeValue)
         #data_dict['Qop1_blocking'].append(Qop1_blocking)
         car0_controller_name = config_car0_controller.childNodes[1].childNodes[0].data
-        data_dict['controller_name'].append(car0_controller_name)
+        car1_controller_name = config_car1_controller.childNodes[1].childNodes[0].data
+        data_dict['car0_controller_name'].append(car0_controller_name)
+        data_dict['car1_controller_name'].append(car1_controller_name)
 
         blocking = eval(config_car0_controller.attributes['blocking_control'].nodeValue)
         data_dict['blocking_control'].append(blocking)
+
+        try:
+            car0_Qop1 = eval(config_car0_controller.attributes['Qop1'].nodeValue)
+            car0_Qop2 = eval(config_car0_controller.attributes['Qop2'].nodeValue)
+        except KeyError:
+            car0_Qop1 = 0
+            car0_Qop2 = 0
+        data_dict['car0_Qop1'].append(car0_Qop1)
+        data_dict['car0_Qop2'].append(car0_Qop2)
+
+        try:
+            car1_Qop1 = eval(config_car1_controller.attributes['Qop1'].nodeValue)
+            car1_Qop2 = eval(config_car1_controller.attributes['Qop2'].nodeValue)
+        except KeyError:
+            car1_Qop1 = 0
+            car1_Qop2 = 0
+        data_dict['car1_Qop1'].append(car1_Qop1)
+        data_dict['car1_Qop2'].append(car1_Qop2)
 
         try:
             a0 = eval(config_car0_controller.attributes['alpha'].nodeValue)
@@ -76,16 +99,6 @@ with open(log,'r') as f:
         data_dict['a0'].append(a0)
         data_dict['a1'].append(a1)
 
-        '''
-        try:
-            q0 = eval(config_car0_controller.attributes['Qop1'].nodeValue)
-            q1 = eval(config_car1_controller.attributes['Qop2'].nodeValue)
-        except KeyError:
-            q0 = None
-            q1 = None
-        data_dict['Qop1'].append(q0)
-        data_dict['Qop2'].append(q1)
-        '''
 
 print(f'failed runs: {failed_runs_count}, total runs: {total_runs}')
 
@@ -96,6 +109,7 @@ for key in data_dict.keys():
 # normalize data
 data_dict['start_lead_i_j'] = (data_dict['start_lead_i_j'] + track.raceline_len_m/2)%track.raceline_len_m - track.raceline_len_m/2
 data_dict['end_lead_i_j'] = (data_dict['end_lead_i_j'] + track.raceline_len_m/2)%track.raceline_len_m - track.raceline_len_m/2
+data_dict['advance'] = data_dict['end_lead_i_j']-data_dict['start_lead_i_j']
 
 
 # table 3: end gain vs lead gain
@@ -126,8 +140,9 @@ def printStats(text,mask):
     boundary_col_j = np.mean(data_dict['boundary_violation_j'][mask])
 
     opponent_col = np.mean(data_dict['opponent_col'][mask])
+    advance = np.mean(data_dict['advance'][mask])
 
-    print(text+f'\t{follow_win_ratio:.2f}\t\t,{lead_win_ratio:.2f}\t\t,{overall_win_ratio:.2f}\t\t,{boundary_col_i:.2f}\t,{boundary_col_j:.2f}\t,{opponent_col:.2f}\t, {np.sum(mask)}')
+    print(text+f'\t{follow_win_ratio:.3f}\t,{lead_win_ratio:.3f}\t,{overall_win_ratio:.3f}\t,{boundary_col_i:.3f}\t,{boundary_col_j:.3f}\t,{opponent_col:.3f}\t,{advance:.3f}\t, {np.sum(mask)}')
 
 
 def exploit_exploit():
@@ -150,8 +165,8 @@ def ilqgame_mpc():
     #Qop1, Qop2
     print(f'qop0,qop1 \t, follow win \t, lead win \t,overall win \t, i out\t, j out\t, col\t,total runs')
     def printTableEntry(a0,a1):
-        mask_a0 = [val == a0 for val in data_dict['Qop1']]
-        mask_a1 = [val == a1 for val in data_dict['Qop2']]
+        mask_a0 = [val == a0 for val in data_dict['car0_Qop1']]
+        mask_a1 = [val == a1 for val in data_dict['car0_Qop2']]
         mask = np.logical_and(mask_a0,mask_a1)
         printStats(f'q0={a0}, q1={a1}',mask)
     printTableEntry(0,0)
@@ -164,8 +179,8 @@ def ilqgame_single():
     #Qop1, Qop2
     print(f'qop0,qop1 \t, follow win \t, lead win \t,overall win \t, i out\t, j out\t, col\t,total runs')
     def printTableEntry(a0,a1):
-        mask_a0 = [val == a0 for val in data_dict['Qop1']]
-        mask_a1 = [val == a1 for val in data_dict['Qop2']]
+        mask_a0 = [val == a0 for val in data_dict['car0_Qop1']]
+        mask_a1 = [val == a1 for val in data_dict['car0_Qop2']]
         mask = np.logical_and(mask_a0,mask_a1)
         printStats(f'q0={a0}, q1={a1}',mask)
     printTableEntry(0,0)
@@ -210,8 +225,8 @@ def triangle_mpc():
     #Qop1, Qop2
     print(f'qop0,qop1 \t, follow win \t, lead win \t,overall win \t, i out\t, j out\t, col\t,total runs')
     def printTableEntry(a0,a1):
-        mask_a0 = [val == a0 for val in data_dict['Qop1']]
-        mask_a1 = [val == a1 for val in data_dict['Qop2']]
+        mask_a0 = [val == a0 for val in data_dict['car0_Qop1']]
+        mask_a1 = [val == a1 for val in data_dict['car0_Qop2']]
         mask = np.logical_and(mask_a0,mask_a1)
         printStats(f'q0={a0}, q1={a1}',mask)
     printTableEntry(0,0)
@@ -224,8 +239,8 @@ def sine_mpc():
     #Qop1, Qop2
     print(f'qop0,qop1 \t, follow win \t, lead win \t,overall win \t, i out\t, j out\t, col\t,total runs')
     def printTableEntry(a0,a1):
-        mask_a0 = [val == a0 for val in data_dict['Qop1']]
-        mask_a1 = [val == a1 for val in data_dict['Qop2']]
+        mask_a0 = [val == a0 for val in data_dict['car0_Qop1']]
+        mask_a1 = [val == a1 for val in data_dict['car0_Qop2']]
         mask = np.logical_and(mask_a0,mask_a1)
         printStats(f'q0={a0}, q1={a1}',mask)
     printTableEntry(0,0)
@@ -233,6 +248,46 @@ def sine_mpc():
     printTableEntry(4,0)
     printTableEntry(-4,-4)
     printTableEntry(4,4)
+
+def sine_mpc_dense():
+    #Qop1, Qop2
+    print(f'qop0,qop1 \t,flow \t,lead \t,all \t,i out\t, j out\t, col\t,adv\t,total runs')
+    def printTableEntry(a0,a1):
+        mask_a0 = [val == a0 for val in data_dict['Qop1']]
+        mask_a1 = [val == a1 for val in data_dict['Qop2']]
+        mask = np.logical_and(mask_a0,mask_a1)
+        printStats(f'q0={a0}, q1={a1}',mask)
+    printTableEntry(0,0)
+    printTableEntry(-4,-4)
+    printTableEntry(4,4)
+
+def triangle_mpc_dense():
+    return sine_mpc_dense()
+def nascar_mpc_dense():
+    return sine_mpc_dense()
+
+def exploit_sine_dense():
+    print(f'qop0,qop1 \t,flow \t,lead \t,all\t,i out\t, j out\t, col\t,adv\t,total runs')
+    def printTableEntry(a0,a1,use_mpc):
+        mask_a0 = [val == a0 for val in data_dict['car1_Qop1']]
+        mask_a1 = [val == a1 for val in data_dict['car1_Qop2']]
+        mask = np.logical_and(mask_a0,mask_a1)
+        if (use_mpc):
+            mask_mpc = [val == 'iLQGameSoloCarController' for val in data_dict['car1_controller_name']]
+        else:
+            mask_mpc = [val != 'iLQGameSoloCarController' for val in data_dict['car1_controller_name']]
+        mask = np.logical_and(mask,mask_mpc)
+        printStats(f'{a0}/{a1} {"mpc" if use_mpc else "GT"}\t',mask)
+    printTableEntry(0,0,True)
+    printTableEntry(0,0,False)
+    printTableEntry(-4,-4,False)
+    printTableEntry(4,4, False)
+
+def exploit_triangle_dense():
+    return exploit_sine_dense()
+def exploit_nascar_dense():
+    return exploit_sine_dense()
+    exploit_sine_dense()
 
 if __name__=='__main__':
     eval(name+'()')
