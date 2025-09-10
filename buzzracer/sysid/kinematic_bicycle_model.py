fix bugs in curvature sign and re-derived from scratch the kinematic bicycle model, now kbm behave as expected

buzzracer/sysid/kinematic_bicycle_model.py
@@ -64,34 +64,30 @@ class KinematicBicycleModelFrenet(VehicleDynamics):
             curvature: signed curvature of ref curve, ccw positive (only used for CurvilinearState)
         Return:
             state at next timestep
-        Ref: https://arxiv.org/pdf/2301.04316
-        Ref: Vehicle Dynamics and Control, 2nd Edition, Sec 2.2
         '''
 
-        # Origin at CG
+        # Origin at CG, beta is the angle between CG velocity and car orientation
         beta = np.arctan(np.tan(control.steering) * car.lr / (car.lf + car.lr))
-        # Do not consider rel_omega in kinematic model
-        dphidt = state.v_forward * (
-            np.cos(beta) / (car.lf + car.lr) * np.tan(control.steering)
-            - curvature*np.cos(state.rel_heading) /
-            (1-curvature*state.lateral_err)
-        )
 
-        dsdt = state.v_forward * np.cos(state.rel_heading)/(1-state.lateral_err*curvature)
-        dndt = state.v_forward * np.sin(state.rel_heading)
-        # Origin at rear axle center
-        # dphidt = state.v_forward * (
-        #     np.tan(control.steering) / (car.lf + car.lr)
-        #     - curvature*np.cos(state.rel_heading) /
-        #     (1-curvature*state.lateral_err)
-        # )
-        dvdt = 6.17 * (control.throttle - state.v_forward / 15.2 - 0.333)
+        dsdt = (state.v_forward * np.cos(state.heading_err) - state.v_sideway *
+                np.sin(state.heading_err))/(1-state.lateral_err*curvature)
+        dndt = state.v_forward * np.sin(state.heading_err) + \
+            state.v_sideway * np.cos(state.heading_err)
+        # acceleration at rear wheel
+        acc_rw = 6.17 * (control.throttle - state.v_forward / 15.2 - 0.333)
+        acc_cg = acc_rw / np.cos(beta)
+        d_v_forward_dt = acc_cg * np.cos(beta)
+        d_v_sideway_dt = acc_cg * np.sin(beta)
+
+        total_v = np.sqrt(state.v_forward**2 + state.v_sideway**2)
+        d_heading_dt = total_v / car.lr * np.sin(beta)
+        d_rel_heading_dt = d_heading_dt - curvature * dsdt
 
         return CurvilinearState(
             progress=state.progress + dsdt * dt,
             lateral_err=state.lateral_err + dndt * dt,
-            rel_heading=state.rel_heading + dphidt * dt,
-            v_forward=state.v_forward + dvdt * dt,
-            v_sideway=0,
+            heading_err=state.heading_err + d_rel_heading_dt * dt,
+            v_forward=state.v_forward + d_v_forward_dt * dt,
+            v_sideway=state.v_sideway + d_v_sideway_dt,
             rel_omega=0
         )

buzzracer/tracks/rcp_track.py
@@ -1186,8 +1186,14 @@ class RCPTrack(Track):
         # radius of curvature can be calculated as R = |y'|^3/sqrt(|y'|^2*|y''|^2-(y'*y'')^2)
         # gives right sign for omega,
         # this is indep of track direction since it's calculated based off vehicle orientation
-        curvature_vec = 1.0/(_norm(dr)**3/(_norm(dr)**2*_norm(ddr)
-                                           ** 2 - np.sum(dr*ddr, axis=0)**2)**0.5)
+        # for magnitude only
+        # curvature_vec = 1.0/(_norm(dr)**3/(_norm(dr)**2*_norm(ddr)
+        #                                    ** 2 - np.sum(dr*ddr, axis=0)**2)**0.5)
+        dx = dr[0]
+        dy = dr[1]
+        ddx = ddr[0]
+        ddy = ddr[1]
+        curvature_vec = (dx * ddy - ddx * dy) / (dx**2 + dy**2)**1.5
         tck, _ = splprep([curvature_vec], u=xx, s=0, per=1)
         self.curvature_s = lambda s: splev(s % self.raceline_len_m, tck)[0]
         return

buzzracer/tracks/track.py
@@ -33,7 +33,7 @@ class Track(ConfigObject):
         self.precise_track_boundary: Callable = lambda coord, heading: (0, 0)
         ''' left, right = self.precise_track_boundary(coord, heading) '''
         self.curvature_s = lambda s: 0.0
-        ''' Function to map track progress to signed curvature of raceline, 
+        ''' Function to map track progress to signed curvature of raceline, Positive is curving left 
         The tck coefficients from splprep, use as curvature = self.curvature_s(s_m)'''
 
         self.ss: np.ndarray = np.array(0)
@@ -353,7 +353,7 @@ class Track(ConfigObject):
         phi = wrap(cart.heading - np.arctan2(dr[1], dr[0]))
         return CurvilinearState(progress=s,
                                 lateral_err=n,
-                                rel_heading=phi,
+                                heading_err=phi,
                                 v_forward=cart.v_forward,
                                 v_sideway=cart.v_sideway,
                                 rel_omega=cart.omega)
@@ -378,7 +378,7 @@ class Track(ConfigObject):
         A = np.array([[0, -1], [1, 0]])
         x, y = r + (A @ dr)*curv.lateral_err
         ref_heading = np.arctan2(dr[1], dr[0])
-        heading = wrap(curv.rel_heading + ref_heading)
+        heading = wrap(curv.heading_err + ref_heading)
         return CartesianState(x=x,
                               y=y,
                               heading=heading,