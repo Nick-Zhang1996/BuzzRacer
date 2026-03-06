02117d562f886120240ca1edfae1441650036bdd
Nick Zhang <zzhang615@ae.gatech.edu>
2026-03-06 15:48:34 -0500

    use consistent discretization steps

--- buzzracer/tracks/nascar_track.py
+++ buzzracer/tracks/nascar_track.py
@@ -42,4 +42,4 @@ class NascarTrack(CurvilinearTrack):
         width = 0.8
         left_margin = np.ones(N) * width
         right_margin = np.ones(N) * width
-        CurvilinearTrack.build_continuous_track(r, left_margin, right_margin)
+        self.data = CurvilinearTrack.build_continuous_track(r, left_margin, right_margin)
